393e540712ea1384e230950c58d9177ac3cf34a6
Marc Morales Aparicio <marcmorales2204@hotmail.com>
2015-05-28 15:01:18 +0200

    changed references

---
+++ MongoDBLibrary/DataBase.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using MongoDB.Driver.Linq;
+using GeoJSON;
+using GeoJSON.Net.Geometry;
+using GeoJSON.Net.Converters;
+using MongoDB.Driver.GeoJsonObjectModel;
+
+namespace Classes
+{
+    public class DataBase
+    {
+        public string connectionString;
+        public MongoClient client;
+        public MongoServer server;
+        public MongoDatabase database;
+        public MongoCollection<Entity> collection;
+
+        public DataBase() // A DataBase constructor which sets the main parameters to connect to MongoDB
+        {
+            this.connectionString = "mongodb://127.0.0.1";
+            this.client = new MongoClient(connectionString);
+            this.server = client.GetServer();
+            this.database = server.GetDatabase("DatosAereos");
+            this.collection = database.GetCollection<Entity>("log");
+        }
+
+        public void AddLog(String address, double value, double latitude, double longitude) // Adds a single log to the DB
+        {
+            // We initialize the database
+            DataBase db = new DataBase();
+
+            // This builds a new instance
+            Entity e = new Entity();
+            e.Address = address;
+            e.Value = value;
+            e.Position = new GeoJsonPoint<GeoJson2DGeographicCoordinates>(new GeoJson2DGeographicCoordinates(longitude, latitude));
+            e.Time = DateTime.Now.AddHours(1);
+            e.SearchTime = (e.Time.Hour - 1) * 3600 + e.Time.Minute * 60 + e.Time.Second;
+            e.ImageName = (e.Time.Day * 1000000000000) + (e.Time.Month * 10000000000) + (e.Time.Year * 1000000) + ((e.Time.Hour - 2) * 10000) + (e.Time.Minute * 100) + (e.Time.Second);
+
+            // We insert the entity into the DB's collection
+            db.collection.Insert(e);
+            var id = e.Id;
+        }
+
+        public void AddLogs(List<Entity> LogList) // Adds multiple logs to the DB
+        {
+            // We initialize the database
+            DataBase db = new DataBase();
+
+            // This loops through the list entered so that every Entity is added
+            for (int i = 0; i < LogList.Count; i++)
+            {
+                // This builds a new instance
+                Entity e = new Entity();
+                e.Address = LogList.ElementAt(i).Address;
+                e.Value = LogList.ElementAt(i).Value;
+                e.Position = LogList.ElementAt(i).Position;
+                e.Time = LogList.ElementAt(i).Time;
+                e.ImageName = LogList.ElementAt(i).ImageName;
+
+                // This adds it to MongoDB
+                db.collection.Insert(e);
+                var id = e.Id;
+            }
+        }
+
+        public List<Entity> SearchByTime(double LowTime, double HighTime)
+        {
+            // We initialize the database
+            DataBase db = new DataBase();
+
+            // This creates the query to search for addresses within the desired time interval
+            var query = Query.And(Query.GTE("SearchTime", LowTime), Query.LTE("SearchTime", HighTime));
+            var resultsCursor = db.collection.Find(query).SetSortOrder("SearchTime");
+
+            // This sends the results to a list
+            var results = resultsCursor.ToList();
+
+            // This returns the list
+            return results;
+
+        }
+
+        public List<Entity> SearchByAddress(string address)
+        {
+            // We initialize the database
+            DataBase db = new DataBase();
+
+            // This creates the query to search for the log(s) with the desired address
+            var query = Query.EQ("Address", address);
+            var resultsCursor = db.collection.Find(query).SetSortOrder("Value");
+
+            // This sends the results to a list
+            var results = resultsCursor.ToList();
+
+            // This returns the list
+            return results;
+        }
+
+        public void RemoveAll()
+        {
+            // We initialize the database
+            DataBase db = new DataBase();
+
+            // This deletes all the content inside the collection
+            db.collection.RemoveAll();
+        }
+
+        public List<Entity> NearQuery(double Lat, double Long)
+        {
+            // We initialize the database
+            DataBase db = new DataBase();
+
+            double distance = 1000;
+            var g = new GeoJson2DGeographicCoordinates(Long, Lat);
+
+            var query = Query.Near<GeoJson2DGeographicCoordinates>("Position", new GeoJsonPoint<GeoJson2DGeographicCoordinates>(g), distance);
+            var resultsCursor = db.collection.Find(query);
+
+            // This sends the results to a list
+            var results = resultsCursor.ToList();
+            return results;
+        }
+        public List<Entity> SearchAll()
+        {
+            // We initialize the database
+            DataBase db = new DataBase();
+
+            // This creates the query to search for the log(s) with the desired address
+            var resultsCursor = db.collection.FindAll();
+
+            // This sends the results to a list
+            var results = resultsCursor.ToList();
+
+            // This returns the list
+            return results;
+        }
+
+        public List<Entity> SearchImageByTime(int option, int lowtime, int hightime)
+        {
+            // We initialize the database
+            DataBase db = new DataBase();
+
+            // Depending on the option chosen, we will have three cases:
+
+            // 1. User wants all images created later than a specified time
+            if (option == 1)
+            {
+                var query = Query.GTE("ImageName", lowtime);
+                var resultsCursor = db.collection.Find(query).SetSortOrder("ImageName");
+
+                // This sends the results to a list
+                var results = resultsCursor.ToList();
+
+                // This returns the list
+                return results;
+            }
+
+            // 2. User wants all images prior to a specified time
+            else if (option == 2)
+            {
+                var query = Query.LTE("ImageName", hightime);
+                var resultsCursor = db.collection.Find(query).SetSortOrder("ImageName");
+
+                // This sends the results to a list
+                var results = resultsCursor.ToList();
+
+                // This returns the list
+                return results;
+            }
+
+            // 3. User wants all images created between two times
+            else
+            {
+                var query = Query.And(Query.GTE("ImageName", lowtime), Query.LTE("ImageName", hightime));
+                var resultsCursor = db.collection.Find(query).SetSortOrder("ImageName");
+
+                // This sends the results to a list
+                var results = resultsCursor.ToList();
+
+                // This returns the list
+                return results;
+            }
+        }
+
+    }
+}
---
+++ MongoDBLibrary/Program.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Classes;
+using GeoJSON;
+using GeoJSON.Net.Geometry;
+using GeoJSON.Net.Converters;
+using MongoDB.Driver.GeoJsonObjectModel;
+
+namespace MongoDBLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type the number corresponding to the option you are willing to use:");
+            Console.WriteLine("Type 1 to use AddLog.");
+            Console.WriteLine("Type 2 to use AddLogs.");
+            Console.WriteLine("Type 3 to research by Time.");
+            Console.WriteLine("Type 4 to research by Name.");
+            Console.WriteLine("Type 5 to remove all data stored");
+            Console.WriteLine("Type 6 to check proximity");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                Console.WriteLine("Type an address.");
+                string address = Console.ReadLine();
+                Console.WriteLine("Type a value.");
+                double value = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter the latitude.");
+                double lat = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter the longitude.");
+                double lon = Convert.ToDouble(Console.ReadLine());
+
+                // This creates the database and executes the AddLog function
+                DataBase db = new DataBase();
+                db.AddLog(address, value, lat, lon);
+            }
+            else if (choice == 2)
+            {
+                List<Entity> LogList = new List<Entity>();
+
+                int confirmation = 1;
+
+                Console.WriteLine("Add as much paired addresses and values as you want.");
+
+                // This keeps asking the user for address and value until he wants to stop
+                while (confirmation != 0)
+                {
+                    Entity e = new Entity();
+                    Console.WriteLine("Type an address.");
+                    string address = Console.ReadLine();
+                    e.Address = address;
+                    Console.WriteLine("Type a value.");
+                    double value = Convert.ToDouble(Console.ReadLine());
+                    e.Value = value;
+                    Console.WriteLine("Enter the latitude.");
+                    double lat = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Enter the longitude.");
+                    double lon = Convert.ToDouble(Console.ReadLine());
+                    e.Position = new GeoJsonPoint<GeoJson2DGeographicCoordinates>(new GeoJson2DGeographicCoordinates(lon, lat));
+                    e.Time = DateTime.Now.AddHours(1);
+                    e.SearchTime = (e.Time.Hour - 1) * 3600 + e.Time.Minute * 60 + e.Time.Second;
+                    e.ImageName = (e.Time.Day * 1000000000000) + (e.Time.Month * 10000000000) + (e.Time.Year * 1000000) + ((e.Time.Hour - 2) * 10000) + (e.Time.Minute * 100) + (e.Time.Second);
+                    LogList.Add(e);
+
+                    Console.WriteLine("Do you wish to continue? (Y=1/N=0)");
+                    confirmation = Convert.ToInt32(Console.ReadLine());
+                }
+
+                // This creates the database and executes the AddLogs function
+                DataBase db = new DataBase();
+                db.AddLogs(LogList);
+                Console.WriteLine("The logs have been added to the DataBase");
+
+            }
+
+            else if (choice == 3)
+            {
+                Console.WriteLine("Type the lower time limit, in the format of: hour/minute/second");
+                string low = Convert.ToString(Console.ReadLine());
+
+                string[] lowsum = low.Split('/');
+
+                int lowlimit = Convert.ToInt32(lowsum[0]) * 3600 + Convert.ToInt32(lowsum[1]) * 60 + Convert.ToInt32(lowsum[2]);
+
+                Console.WriteLine("Type the higher value limit, in the format of: hour/minute/second");
+                string high = Convert.ToString(Console.ReadLine());
+
+                string[] highsum = high.Split('/');
+
+                int highlimit = Convert.ToInt32(highsum[0]) * 3600 + Convert.ToInt32(highsum[1]) * 60 + Convert.ToInt32(highsum[2]);
+
+                // This creates the database and executes the SearchByTime function
+                DataBase db = new DataBase();
+                List<Entity> LogList = db.SearchByTime(lowlimit, highlimit);
+
+                int count = LogList.Count;
+
+                // This prints the results of the function in the console
+                for (int i = 0; i < count; i++)
+                {
+                    Entity e = new Entity();
+                    e.Address = LogList.ElementAt(i).Address;
+                    e.Value = LogList.ElementAt(i).Value;
+                    e.Position = LogList.ElementAt(i).Position;
+                    e.Time = LogList.ElementAt(i).Time;
+                    e.SearchTime = LogList.ElementAt(i).SearchTime;
+
+
+                    string print1 = Convert.ToString(e.Address);
+                    string print2 = Convert.ToString(e.Value);
+                    string print3 = Convert.ToString(e.Position.Coordinates.Latitude);
+                    string print4 = Convert.ToString(e.Position.Coordinates.Longitude);
+                    string print5 = Convert.ToString(e.Time);
+                    string print6 = Convert.ToString(e.SearchTime);
+
+                    Console.WriteLine("Address: {0}, Value: {1}, Position: Latitude: {2} Longitude: {3}, Time: {4}, SearchTime: {5}", print1, print2, print3, print4, print5, print6);
+                }
+            }
+
+            else if (choice == 4)
+            {
+                Console.WriteLine("Type the name you wish to search for");
+                string name = Convert.ToString(Console.ReadLine());
+
+                // This creates the database and executes the SearchByAddress function
+                DataBase db = new DataBase();
+                List<Entity> LogList = db.SearchByAddress(name);
+
+                int count = LogList.Count;
+
+                // This prints the results of the function in the console
+                for (int i = 0; i < count; i++)
+                {
+                    Entity e = new Entity();
+                    e.Address = LogList.ElementAt(i).Address;
+                    e.Value = LogList.ElementAt(i).Value;
+                    e.Position = LogList.ElementAt(i).Position;
+                    e.Time = LogList.ElementAt(i).Time;
+                    e.SearchTime = LogList.ElementAt(i).SearchTime;
+
+                    string print1 = Convert.ToString(e.Address);
+                    string print2 = Convert.ToString(e.Value);
+                    string print3 = Convert.ToString(e.Position);
+                    string print4 = Convert.ToString(e.Time);
+                    string print5 = Convert.ToString(e.SearchTime);
+
+                    Console.WriteLine("Address: {0}, Value: {1}, Position: {2}, Time: {3}, SearchTime: {4}", print1, print2, print3, print4, print5);
+                }
+            }
+
+            else if (choice == 5)
+            {
+                // This creates the database and executes the RemoveAll function
+                DataBase db = new DataBase();
+                db.RemoveAll();
+            }
+
+            else if (choice == 6)
+            {
+                // The user enters the point to be checked for proximity
+                Console.WriteLine("Insert the point you want to be checked:");
+                Console.WriteLine("Enter its latitude");
+                double latpoint = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter its longitude");
+                double longpoint = Convert.ToDouble(Console.ReadLine());
+
+                // A database is initialized and the function to check the MongoDB is executed
+                DataBase db = new DataBase();
+                List<Entity> NearList = db.NearQuery(latpoint, longpoint);
+
+                int count = NearList.Count;
+
+                // This prints the results of the function in the console
+                for (int i = 0; i < count; i++)
+                {
+                    Entity e = new Entity();
+                    e.Address = NearList.ElementAt(i).Address;
+                    e.Value = NearList.ElementAt(i).Value;
+                    e.Position = NearList.ElementAt(i).Position;
+                    e.Time = NearList.ElementAt(i).Time;
+                    e.SearchTime = NearList.ElementAt(i).SearchTime;
+
+                    string print1 = Convert.ToString(e.Address);
+                    string print2 = Convert.ToString(e.Value);
+                    string print3 = Convert.ToString(e.Position.Coordinates.Latitude);
+                    string print4 = Convert.ToString(e.Position.Coordinates.Longitude);
+                    string print5 = Convert.ToString(e.Time);
+                    string print6 = Convert.ToString(e.SearchTime);
+
+                    Console.WriteLine("The points within its established vicinity are:");
+                    Console.WriteLine("Address: {0}, Value: {1}, Position: Latitude: {2} Longitude: {3}, Time: {4}, SearchTime: {5}", print1, print2, print3, print4, print5, print6);
+                }
+            }
+        }
+    }
+}
